fbcc4a0131ab97e00f8135c454587e2b3f76ee3c
Zach Moore <zach@samcart.com>
2019-01-08 23:53:51 -0500

    start sign in page

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { func } from 'prop-types';
+
+import '../styles/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        Login page
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = () => ({});
+
+const matchDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, matchDispatchToProps)(App);
